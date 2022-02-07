6be551cc5b311fc4561c5b4fd4f220be2262fb6a
Janusred <horaciok300@hotmail.com>
2022-02-07 00:07:47 -0600

    Agregando imagenes con Ramdom

--- src/index.js
+++ src/index.js
@@ -6,15 +6,35 @@
 console.log('Janus!! Todo va genail!! :)')
 
 
+const minimum = 1;
+const maximum = 122;
+const random= () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
+ 
 const createImageNode =()=>{
+
+    const container= document.createElement("div");
+    container.className="p-4"
+
     const imagen = document.createElement('img')
     imagen.className="mx-auto";
     imagen.width="320";
-    imagen.src="https://randomfox.ca/images/50.jpg";
+    imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
 
-    return imagen;
+
+    container.appendChild(imagen);
+
+    return container;
 };
 
  const nuevaImagen = createImageNode();
  const mountNode =document.getElementById("images");
   mountNode.appendChild(nuevaImagen);
+
+  const addButton = document.querySelector("Button");
+
+   const addImage=()=>{
+       const newImage= createImageNode();
+       mountNode.append(newImage);
+   }
+  
+  addButton.addEventListener("click",addImage);
