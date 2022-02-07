72a1a36e8ad266d885d0cfbb460a9f443daa8abd
Janusred <horaciok300@hotmail.com>
2022-02-06 23:37:44 -0600

    Pruebas de api

--- src/index.js
+++ src/index.js
@@ -3,4 +3,18 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+console.log('Janus!! Todo va genail!! :)')
+
+
+const createImageNode =()=>{
+    const imagen = document.createElement('img')
+    imagen.className="mx-auto";
+    imagen.width="320";
+    imagen.src="https://randomfox.ca/images/50.jpg";
+
+    return imagen;
+};
+
+ const nuevaImagen = createImageNode();
+ const mountNode =document.getElementById("images");
+  mountNode.appendChild(nuevaImagen);
