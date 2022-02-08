b74b8606139253f62bcc401a3278189f4479dbeb
Janusred <horaciok300@hotmail.com>
2022-02-07 22:20:24 -0600

    lazy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+
+import { registerImage } from "./lazy"; 
+
+
+
 /**
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
