e05e93f30915b296bb9ba903a06d88853f71c01d
Janusred <Janusred@users.noreply.github.com>
2022-02-08 06:06:47 +0000

    deploy: e274feb26eed8e4ef0b90c0e27ba49dec34b4dc3

--- _dist_/lazy.js
+++ _dist_/lazy.js
@@ -1,3 +1,17 @@
-const registerImage = (imagen)=> {
-    
-}
+const isInterersecting = (entry) =>{
+return entry.isInterersecting;
+};
+
+const accion =(entry)=>{
+    const nodo = entry.target;
+    console.log("Hola Janus!!");
+
+    observer.unobserve(nodo);
+};
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.filter(isInterersecting).forEach(accion);
+});
+export const registerImage = (imagen)=> {
+    observer.observe(imagen);
+};
